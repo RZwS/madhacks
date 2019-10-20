ce5c82e7cbc7663132ae26bbef10cec9e95123b7
孙楠 <sunnan@s-MacBook-puro.local>
2019-10-19 22:26:29 -0500

    drag

--- js/assist.js
+++ js/assist.js
@@ -1,3 +1,22 @@
-function drag(){
+
+
+function dragFromRight(e){
+ var id =e.target.id.substr(e.target.id.length - 1, 1)
+
+ if(e.onclick){
+
+    var image = $("#"+id).prop("background-image");
+    $("#column-wrap").append(
+        "<div id='draging' class='dragItem'><img src='" +image+"' id='dragImage'><img></div>");
+
+    
+    
+     $("#"+id).css("background-image",null);
+
+    
+
+}
+  
    
+
 }
